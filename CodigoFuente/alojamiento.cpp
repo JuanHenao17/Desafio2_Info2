076268c751f3b9620f1e40e6835126031605e060
SamuelZuleta <samuel.zuleta1@udea.edu.co>
2025-05-22 00:03:56 -0500

    Estructura base clase Alojamiento

--- CodigoFuente/alojamiento.cpp
+++ CodigoFuente/alojamiento.cpp
@@ -1,3 +1,39 @@
 #include "alojamiento.h"
+#include <string>
 
-alojamiento::alojamiento() {}
+Alojamiento::Alojamiento() {
+    codigo = 0;
+    nombre = "";
+    tipo = "";
+    direccion = "";
+    municipio = "";
+    departamento = "";
+    precioNoche = 0;
+    for (int i = 0; i < 6; i++) amenidades[i] = false;
+    reservaciones = nullptr;
+
+}
+
+Alojamiento::Alojamiento(unsigned int cod, const string nom, const string tip, const string dir,
+                         const string muni, const string depto, double precio, const bool amen[]) {
+    codigo = cod;
+    nombre = nom;
+    tipo = tip;
+    direccion = dir;
+    municipio = muni;
+    departamento = depto;
+    precioNoche = precio;
+    for (int i = 0; i < 6; i++) amenidades[i] = amen[i];
+    reservaciones = nullptr;
+
+}
+
+int Alojamiento::getCodigo() const { return codigo; }
+const string Alojamiento::getNombre() const { return nombre; }
+const string Alojamiento::getTipo() const { return tipo; }
+const string Alojamiento::getDireccion() const { return direccion; }
+const string Alojamiento::getMunicipio() const { return municipio; }
+const string Alojamiento::getDepartamento() const { return departamento; }
+double Alojamiento::getPrecioNoche() const { return precioNoche; }
+bool* Alojamiento::getAmenidades() { return amenidades; }
+
--- CodigoFuente/alojamiento.h
+++ CodigoFuente/alojamiento.h
@@ -1,10 +1,41 @@
 #ifndef ALOJAMIENTO_H
 #define ALOJAMIENTO_H
+#include <string>
+#include "reservacion.h"
 
-class alojamiento
-{
+using namespace std;
+
+class Alojamiento {
+private:
+    unsigned int codigo;
+    string nombre;
+    string tipo;         // "casa" o "apartamento"
+    string direccion;
+    string municipio;
+    string departamento;
+    double precioNoche;
+    bool amenidades[6];    // ascensor, piscina, aire, caja, parqueadero, patio
+
+    Reservacion** reservaciones;
+
 public:
-    alojamiento();
+    Alojamiento();
+    Alojamiento(unsigned int cod, const string nom, const string tipo, const string dir,
+                const string muni, const string depto, double precio, const bool amen[]);
+
+    // Getters
+    int getCodigo() const;
+    const string getNombre() const;
+    const string getTipo() const;
+    const string getDireccion() const;
+    const string getMunicipio() const;
+    const string getDepartamento() const;
+    double getPrecioNoche() const;
+    bool* getAmenidades();
+    Reservacion** getReservaciones() const;
+
+
 };
 
-#endif // ALOJAMIENTO_H
+#endif
+
